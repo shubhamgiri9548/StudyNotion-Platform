c59ebe98a75573cad93c77d71b118ee42747eb5c
shubhamgiri9548 <ankushgiri2007@gmail.com>
2025-04-26 21:05:16 +0530

    change in ind

--- server/index.js
+++ server/index.js
@@ -23,9 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        // origin: "http://localhost:3000",
-        // frotned url
-        origin: "https://study-notion-platform-egjg.vercel.app", 
+        origin: "http://localhost:3000",
         credentials: true,
     })
 )
