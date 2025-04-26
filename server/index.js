842e802e3d9bcd8e71e33a0c05df51a1217de328
shubhamgiri9548 <ankushgiri2007@gmail.com>
2025-04-26 17:35:18 +0530

    some

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(
     cors({
         // origin: "http://localhost:3000",
+        // frotned url
         origin: "https://study-notion-platform-sntl.vercel.app",
         credentials: true,
     })
